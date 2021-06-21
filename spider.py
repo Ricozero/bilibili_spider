5dd9b3c033a3f7a872b3f4a073e5b3a066544309
Ricozero <735784247@qq.com>
2021-06-21 13:58:12 +0800

    实现所有剧集热门评论的导出

--- spider.py
+++ spider.py
@@ -1,5 +1,7 @@
 import requests
 from pprint import pprint
+from tqdm import tqdm
+import json
 
 dn = 'https://api.bilibili.com' # domain name
 
@@ -25,11 +27,49 @@ def get_video_comments(oid, sort='hot'):
         raise ValueError('Wrong sort argument.')
     return r.json()
 
-basics = get_season_basics(28229010)
-pprint(basics)
-details = get_sesason_details(basics['result']['media']['season_id'])
-#pprint(details)
-scom = [] # season comments
-for ep in details['result']['episodes']:
-    scom.append(get_video_comments(ep['aid']))
-#pprint(scom)
+def dump_season_comments(mid):
+    results = {}
+    basics = get_season_basics(mid)
+    results['basics'] = basics
+    details = get_sesason_details(basics['result']['media']['season_id'])
+    results['details'] = details
+    comments = []
+    for ep in tqdm(details['result']['episodes']):
+        comments.append(get_video_comments(ep['aid']))
+    results['comments'] = comments
+    with open(str(mid) + '.json', 'w') as f:
+        f.write(json.dumps(results))
+
+def length(str):
+    # length of a string, chinese characters counted as 2
+    str_utf8 = str.encode('utf-8')
+    return int((len(str_utf8) + len(str)) / 2)
+
+def process_season_comments(mid):
+    try:
+        f = open(str(mid) + '.json', 'r')
+    except IOError:
+        dump_season_comments(mid)
+    results = json.loads(f.read())
+    f.close()
+
+    f = open(str(mid) + '_comments.txt', 'w', encoding='utf-8')
+    # iterate over replies of all episodes
+    for i, epr in enumerate(results['comments']):
+        title = results['details']['result']['episodes'][i]['long_title']
+        s = '%d、%s' % (i + 1, title)
+        l = length(s)
+        if l % 2 == 1:
+            l += 1
+            s += ' '
+        f.write('╔' + '═' * l + '╗\n')
+        f.write('║%s ║\n' % s)
+        f.write('╚' + '═' * l + '╝\n\n')
+        for reply in epr['data']['replies']:
+            uname = reply['member']['uname']
+            content = reply['content']['message']
+            f.write('[%s]\n%s\n\n' % (uname, content))
+        f.write('\n')
+    f.close()
+
+process_season_comments(28229010)
